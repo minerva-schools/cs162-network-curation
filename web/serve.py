c2791e43aee6d2bb2299fa261b4f2f4a9c3b7140
Verina Armanyous <verina.samir99@gmail.com>
2021-03-01 17:12:18 -0800

    Postgres connection on Heroku

--- web/serve.py
+++ web/serve.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jiseufnjbxslue:05457b355e4aabeaeae4a1c17e76c07fde2b7a91c702f91e9be4d1d270d36817@ec2-3-211-245-154.compute-1.amazonaws.com:5432/ddd507tv8dg7mc'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -14,19 +14,23 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
 
+
 db.create_all()
 example_user = User(id=1, name="Philip Sterne")
 db.session.merge(example_user)
 db.session.commit()
 
-@app.route('/')
+
+@ app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/users')
+
+@ app.route('/users')
 def users():
     users = User.query.all()
     return render_template('users.html', users=users)
+
 
 if __name__ == '__main__':
     app.run()
