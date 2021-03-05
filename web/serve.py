d04d47dabca1d6362b737090734aebc60409e46b
Parker Cline <parker.cline@minerva.kgi.edu>
2021-03-05 16:22:00 -0500

    checks correctly if username/password are in db

--- web/serve.py
+++ web/serve.py
@@ -9,17 +9,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+login = LoginManager(app)
+login.init_app(app)
+login.login_view = 'login'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
     username = db.Column(db.String(200), unique=True)
-    password = db.Column(db.String(200))
+    password_hash = db.Column(db.String(128))
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -28,11 +31,17 @@ class User(db.Model):
         return check_password_hash(self.password_hash, password)
 
 db.create_all()
-example_user = User(id=1, name="Philip Sterne", username="username", password="password")
+example_user = User(id=1, name="Philip Sterne", username="username")
+example_user.set_password('mypassword')
+
 db.session.merge(example_user)
 db.session.commit()
 
-@app.route('/')
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = LoginForm()
     return render_template('index.html', form=form)
@@ -42,10 +51,22 @@ def users():
     users = User.query.all()
     return render_template('users.html', users=users)
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    print("Logging in")
+    #if current_user.is_authenticated:
+        #print("Authenticated")
+       #return redirect(url_for('index'))
     form = LoginForm()
-    return render_template('index.html', title='Sign In', form=form)
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('index'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('main'))
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
