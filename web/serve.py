625db690bb9cadf349a45aeb113f3805e4ac2800
Parker Cline <parker.cline@minerva.kgi.edu>
2021-03-05 14:44:56 -0500

    login form now passes through information to main

--- web/serve.py
+++ web/serve.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, LoginManager
+from flask_login import login_user, LoginManager, UserMixin, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 import os
@@ -21,13 +21,6 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(200), unique=True)
     password = db.Column(db.String(200))
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-
 @login_manager.user_loader
 def user_loader(user_id):
     """Given *user_id*, return the associated User object.
@@ -37,7 +30,6 @@ def user_loader(user_id):
     """
     return User.query.get(user_id)
 
-
 db.create_all()
 example_user = User(id=1, name="Philip Sterne", username="username", password="password")
 db.session.merge(example_user)
@@ -46,6 +38,12 @@ db.session.commit()
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    user = User(username=request.form['loginusername'], password=request.form['loginpassword'])
+    print(request.form['loginusername'])
+    return render_template('main.html', current_user=user)
 
 @app.route('/users')
 def users():
