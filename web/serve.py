e0f5900c5e4166e3a9dbdd19e3ec17259307ced9
Parker Cline <parker.cline@minerva.kgi.edu>
2021-03-05 15:34:40 -0500

    implemented wtforms

--- web/serve.py
+++ web/serve.py
@@ -1,34 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_user, LoginManager, UserMixin, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 import os
+from forms import LoginForm
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-login_manager = LoginManager()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 
-
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
     username = db.Column(db.String(200), unique=True)
     password = db.Column(db.String(200))
 
-@login_manager.user_loader
-def user_loader(user_id):
-    """Given *user_id*, return the associated User object.
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-    :param unicode user_id: user_id (email) user to retrieve
-
-    """
-    return User.query.get(user_id)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 db.create_all()
 example_user = User(id=1, name="Philip Sterne", username="username", password="password")
@@ -37,18 +34,19 @@ db.session.commit()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/login', methods=['POST'])
-def login():
-    user = User(username=request.form['loginusername'], password=request.form['loginpassword'])
-    print(request.form['loginusername'])
-    return render_template('main.html', current_user=user)
+    form = LoginForm()
+    return render_template('index.html', form=form)
 
 @app.route('/users')
 def users():
     users = User.query.all()
     return render_template('users.html', users=users)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('index.html', title='Sign In', form=form)
+
 
 if __name__ == '__main__':
     app.run()
