b3334c683e8b86bf875f7ec3a53940869ad77194
ldiima <dima@minerva.kgi.edu>
2021-03-12 17:14:50 -0800

    import dependencies

--- web/models.py
+++ web/models.py
@@ -1,4 +1,16 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_user, LoginManager, UserMixin, current_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+import os
+from .forms import LoginForm
+from dotenv import load_dotenv
+load_dotenv()
+app = Flask(__name__)
+login = LoginManager(app)
+login.init_app(app)
+login.login_view = 'login'
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
