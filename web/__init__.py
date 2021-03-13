a823792f89335ac9cc6b2aa12fd610bcff1b6b13
ldiima <dima@minerva.kgi.edu>
2021-03-12 18:37:26 -0800

    models.py moved from serve.py

--- web/__init__.py
+++ web/__init__.py
@@ -1,1 +1,2 @@
 from .serve import app, db
+from .models import app, db
--- web/serve.py
+++ web/serve.py
@@ -16,29 +16,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-db = SQLAlchemy(app)
-
-
-db.create_all()
-example_user = User(id=1, name="Philip Sterne", username="username")
-example_user.set_password('mypassword')
-
-db.session.merge(example_user)
-db.session.commit()
-db.create_all()
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
-    username = db.Column(db.String(200), unique=True)
-    password_hash = db.Column(db.String(128))
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -51,7 +28,7 @@ def index():
 
 
 
-@app.route('/users')
+@ app.route('/users')
 def users():
     users = User.query.all()
     return render_template('users.html', users=users)
