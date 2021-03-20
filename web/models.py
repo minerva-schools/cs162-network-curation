b03030c7aa386b2609fec665d51a6be10057d4a6
Tiago Flora <tiagomflora@gmail.com>
2021-03-20 13:47:57 -0700

    Include WTForms and Flask Login in requirements, fix font name typo, and add docstring to models.py

--- web/models.py
+++ web/models.py
@@ -1,3 +1,18 @@
+"""
+Define object models for the application
+
+Current models:
+    - User:
+        - Attributes:
+            - ID
+            - Name
+            - Username
+            - Password hash
+        - Methods:
+            - Set password
+            - Check password
+
+"""
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
