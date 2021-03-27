44bb03ee6420a159915afd566284fce57ec0c0a9
Parker Cline <parker.cline@minerva.kgi.edu>
2021-03-27 17:02:54 -0400

    created Note and UserNotes table. foreign keys.

--- web/models.py
+++ web/models.py
@@ -20,6 +20,7 @@ from .serve import db
 
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
     username = db.Column(db.String(200), unique=True)
@@ -33,6 +34,17 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-class UserConnection(UserMixin, db.Model):
-    id1 = db.Column(db.Integer, primary_key=True)
-    id2 = db.Column(db.Integer)
+class Note(UserMixin, db.Model):
+    __tablename__ = 'notes'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(600))
+    locationmet = db.Column(db.String(200))
+    label = db.Column(db.String(10))
+
+
+class UserNotes(UserMixin, db.Model):
+    __tablename__ = 'usernotes'
+    userid = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    noteid = db.Column(db.Integer, db.ForeignKey('notes.id'), primary_key=True)
+    users = db.relationship('User', foreign_keys=userid)
+    notes = db.relationship('Note', foreign_keys=noteid)
