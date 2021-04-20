1d1cce792f26f06e4a259307f9bb044dffdc8804
TiagoFlora <tiagomflora@gmail.com>
2021-04-20 10:24:14 -0700

    change imports for consistency

--- unit_test/test_user_model.py
+++ unit_test/test_user_model.py
@@ -2,7 +2,7 @@ import os
 import sys
 topdir = os.path.join(os.path.dirname(__file__), "..")
 sys.path.append(topdir)
-from web.models import User, UserConnection
+from web.models import Users, UserConnections
 from web import create_app, db
 from werkzeug.security import generate_password_hash, check_password_hash
 import unittest
@@ -20,20 +20,20 @@ class UserModelTest(unittest.TestCase):
         self.app_context.pop()
 
     def test_pw_set(self):
-        user = User()
+        user = Users()
         user.set_password('pwd123')
         self.assertTrue(user.password_hash is not None)
 
     def test_pwd_verification(self):
-        user = User()
+        user = Users()
         user.set_password('pwd123')
         self.assertTrue(user.check_password('pwd123'))
         self.assertFalse(user.check_password('pd13'))
 
     def test_pwd_salts_are_random(self):
-        user = User()
+        user = Users()
         user.set_password('pwd123')
-        user2 = User()
+        user2 = Users()
         user2.set_password('pwd123')
         self.assertTrue(user.password_hash != user2.password_hash)
 
