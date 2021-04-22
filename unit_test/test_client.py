1fd7d0d50b394594fd932dd35002c1ba5dae609f
Parker Cline <parker.cline@minerva.kgi.edu>
2021-04-22 15:07:57 -0400

    test_client now works

--- unit_test/test_client.py
+++ unit_test/test_client.py
@@ -1,79 +1,88 @@
-######################################################
-######### Test API with Flask testing client #########
-######################################################
-import unittest
-import re
+# Example taken from:
+# http://flask.pocoo.org/docs/1.0/testing/
+# and suitably modified.
 import os
-import sys
-topdir = os.path.join(os.path.dirname(__file__), "..")
-sys.path.append(topdir)
-# TODO: Fix issue with imports
-from web.models import Users
+import tempfile
+
+import pytest
+
 from web import create_app, db
+from web.models import Users
+
+app = create_app()
 
 
-class TestFlaskClient(unittest.TestCase):
-    def setUp(self):
-        self.app = create_app()
-        self.app.config['TESTING'] = True
-        self.app_context = self.app.app_context()
-        self.app_context.push()
+def signup(client, username, email, passwd):
+    print(passwd)
+    return client.post(
+        "/signup",
+        data=dict(user_name=username, email=email, password=passwd, confirm_password=passwd),
+        follow_redirects=True,
+    )
+
+
+def login(client, email_or_username, password):
+    return client.post(
+        "/login",
+        data=dict(email_or_username=email_or_username, password=password),
+        follow_redirects=True,
+    )
+
+
+def logout(client):
+    return client.get("/logout", follow_redirects=True)
+
+
+@pytest.fixture
+def client():
+    db_fd, app.config["DATABASE"] = tempfile.mkstemp()
+    app.config["TESTING"] = True
+    app.config["WTF_CSRF_ENABLED"] = False
+    # User used for test
+    app.config["USERNAME"] = "parkercline"
+    app.config["EMAIL"] = "parker@parker.com"
+    app.config["PASSWORD"] = "Poodles01$"
+
+    app.app_context().push()
+
+    with app.app_context():
         db.create_all()
-        self.client = self.app.test_client()
+    client = app.test_client()
 
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-        self.app_context.pop()
+    yield client
 
-    def test_home(self):
-        # Home should redirect to login page
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 404)
-        
-    def test_signup(self):
-        # TODO: Figure out why the POST request to signup is returning 404
-        response = self.client.post('/signup', data = {
-            'user_name': 'psterne',
-            'password': 'pwd123',
-            'confirm_password': 'pwd123',
-            'email': 'doe@example.com'
-        })
-        # Should be 302
-        # self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.status_code, 404)
+    os.close(db_fd)
+    os.unlink(app.config["DATABASE"])
 
-    def test_login(self):
-        response = self.client.post('/signup', data = {
-            'user_name': 'psterne',
-            'password': 'pwd123',
-            'confirm_password': 'pwd123',
-            'email': 'doe@example.com'
-        })
-        response = self.client.post('/login', data = {
-            'email_or_username': 'doe@example.com',
-            'password': 'pwd123'
-        }, follow_redirects = True)
-        # Should be 200
-        # self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.status_code, 404)
 
-    def test_logout(self):
-        response = self.client.post('/signup', data = {
-            'user_name': 'psterne',
-            'password': 'pwd123',
-            'confirm_password': 'pwd123',
-            'email': 'doe@example.com'
-        })
-        response = self.client.post('/login', data = {
-            'email_or_username': 'doe@example.com',
-            'password': 'pwd123'
-        }, follow_redirects = True)
-        # TODO: Have an address to get a logout response
-        response = self.client.get('/logout', follow_redirects = True)
-        # Should be 200
-        # self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.status_code, 404)
+def test_signup_login_logout(client):
+    """Make sure login and logout works."""
 
-if __name__ == "__main__":
-    unittest.main()
+    # TODO: ADD Assertion statements to carry out the test
+
+    rv = signup(
+        client, app.config["USERNAME"], app.config["EMAIL"], app.config["PASSWORD"]
+    )
+    assert Users.query.filter_by(name=app.config["USERNAME"]).first() is not None
+
+    # LOGIN VIA USERNAME
+    rv = login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    assert b"Add a Connection" in rv.data
+    assert b"Login" not in rv.data
+
+    rv = logout(client)
+    assert b'Add a Connection' not in rv.data
+
+    # LOGIN VIA EMAIL
+    rv = login(client, app.config["EMAIL"], app.config["PASSWORD"])
+    assert b"Add a Connection" in rv.data
+    assert b"Login" not in rv.data
+
+    rv = logout(client)
+    assert b'Add a Connection' not in rv.data
+
+    rv = login(client, app.config["USERNAME"] + "x", app.config["PASSWORD"])
+    assert b'Invalid email or username' in rv.data
+
+    rv = login(client, app.config["USERNAME"], app.config["PASSWORD"] + "x")
+    assert b'Invalid password' in rv.data
