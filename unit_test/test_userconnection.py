521db930c2f5112e63e72aecf590e9f3d16e2043
Parker Cline <parker.cline@minerva.kgi.edu>
2021-04-23 17:10:49 -0400

    added userconnection tests

---
+++ unit_test/test_userconnection.py
@@ -0,0 +1,58 @@
+# Example taken from:
+# http://flask.pocoo.org/docs/1.0/testing/
+# and suitably modified.
+import os
+import tempfile
+
+import pytest
+
+from web import create_app, db
+from web.models import UserConnections
+from web.serve import get_overdue
+from datetime import date, timedelta 
+
+app = create_app()
+
+
+@pytest.fixture
+def client():
+
+    db_fd, app.config["DATABASE"] = tempfile.mkstemp()
+    app.config["TESTING"] = True
+    app.config["WTF_CSRF_ENABLED"] = False
+
+    app.app_context().push()
+
+    with app.app_context():
+        db.create_all()
+    client = app.test_client()
+
+    yield client
+
+    os.close(db_fd)
+    os.unlink(app.config["DATABASE"])
+
+def test_userconnection_add(client):
+    test_user = UserConnections(
+        id=1,
+        contact_by=date.today(),
+        last_contacted=date.today() - timedelta(days=1),
+    )
+    db.session.add(test_user)
+    db.session.commit()
+    assert UserConnections.query.filter_by(id=1).first() is not None
+
+def test_overdue_checker(client):
+    test_user = UserConnections(
+        id=1,
+        contact_by=date.today(),
+        last_contacted=date.today() - timedelta(days=1),
+    )
+    db.session.add(test_user)
+    db.session.commit()
+    connections = UserConnections.query.all()
+    overdue_connections = get_overdue(connections)
+    assert overdue_connections[0].id is 2
+
+    
+
