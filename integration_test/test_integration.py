403fd554a6ebb4e7167ee269fc2eb3650aca90f5
Parker Cline <parker.cline@minerva.kgi.edu>
2021-04-23 19:19:17 -0400

    integration test now incorporates adding connections and checking overdue connections

--- integration_test/test_integration.py
+++ integration_test/test_integration.py
@@ -9,8 +9,7 @@ import tempfile
 import pytest
 
 from web import create_app, db
-from web.models import Users
-
+from web.models import Users, UserConnections
 app = create_app()
 
 def signup(client, username, email, passwd, confirm_passwd):
@@ -37,6 +36,13 @@ def login(client, email_or_username, password):
 def logout(client):
     return client.get("/logout", follow_redirects=True)
 
+def add_connection(client, name, contact_by, last_contacted):
+    return client.post(
+        "/add_connection",
+        data=dict(name=name, contact_by=contact_by, last_contacted=last_contacted),
+        follow_redirects=True,
+    )
+
 @pytest.fixture
 def client():
 
@@ -60,11 +66,18 @@ def client():
     os.close(db_fd)
     os.unlink(app.config["DATABASE"])
 
+'''
 def test_basic_request():
     r = requests.get("http://127.0.0.1:5000/")
     assert r.status_code == 200
+'''
+from web.serve import get_overdue 
+from datetime import date, timedelta
 
 def test_login(client):
+    db.drop_all()
+    db.create_all()
+
     # valid signup
     rv = signup(
         client,
@@ -87,6 +100,22 @@ def test_login(client):
     rv = login(client, app.config["EMAIL"], app.config["PASSWORD"])
     assert b"Add a Connection" in rv.data
     assert b"Login" not in rv.data
+
+    assert UserConnections.query.filter_by(name="ConnectionAddedIntegration").first() is None
+    assert UserConnections.query.filter_by(name="NotOverdueIntegration").first() is None
+    rv = add_connection(client, "ConnectionAddedIntegration", date.today(), date.today() - timedelta(days=1))
+    assert rv.status_code == 200
+    rv = add_connection(client, "NotOverdueIntegration", date.today() - timedelta(days=1), date.today())
+    assert UserConnections.query.filter_by(name="ConnectionAddedIntegration").first() is not None
+    assert UserConnections.query.filter_by(name="NotOverdueIntegration").first() is not None
+    rv = client.get('/', follow_redirects=True)
+    assert rv.status_code == 200
+
+    connections = UserConnections.query.all()
+    overdue_connections = get_overdue(connections)
+    overdue_names = [connection.name for connection in overdue_connections]
+    assert "ConnectionAddedIntegration" in overdue_names
+    assert "NotOverdueIntegration" not in overdue_names
 
     rv = logout(client)
     assert b"Add a Connection" not in rv.data
